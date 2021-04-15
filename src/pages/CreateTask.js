ed2665cb3384b44e43b417400343bd6931168b2a
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-15 10:35:44 +0300

    creating create task page and user not have an ability to create an task

--- src/pages/CreateTask.js
+++ src/pages/CreateTask.js
@@ -2,18 +2,46 @@ import React,{useState} from 'react'
 import { Container,Row, Col, Form } from 'react-bootstrap'
 import CustomButton from '../components/CustomButton'
 import CustomInput from '../components/CustomInput'
+import {useSelector} from 'react-redux'
+import db from '../firebase'
+import firebase from 'firebase'
+import {toast} from 'react-toastify'
 
-const CreateTask = () => {
-    
+
+const CreateTask = ({history}) => {
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
+
+    const user = useSelector((state)=> state.user)
+    const {curentuser} = user
+
+    const handleTask = (e) => {
+        e.preventDefault()
+        try {
+            db.collection('users').doc(curentuser.id).collection('tasks').add({
+                title: title,
+                description: description,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                complete: false
+            })
+            setTitle('');
+            setDescription('');
+            toast.success('You have created new task successful')
+            history.push('/tasks')
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
     return (
         <div className='create-task-page'>
             <Container>
                 <Row>
                     <Col md={6}>
                     <Form>
-                    <CustomInput  lable='Title'/>
-                    <CustomInput  lable='Discreption'/>
-                    <CustomButton>Add new task</CustomButton>
+                    <CustomInput  lable='Title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <CustomInput  lable='Description' value={description} onChange={(e) => setDescription(e.target.value)}/>
+                    <CustomButton onClick={handleTask}>Add new task</CustomButton>
                     </Form>
                     </Col>
                     <Col md={6}>
